4a5937c5c582c6a22ff1719e6bcdfa1c2300c994
Shubhansu kumar <shubhansu2021@gmail.com>
2022-11-01 12:25:17 +0530

    Day41: dSA
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- DSA/41_Day/00_BinaryTraversal.cpp
+++ DSA/41_Day/00_BinaryTraversal.cpp
@@ -1,28 +1,99 @@
 // https://www.hackerrank.com/contests/cse205-16915-day41/challenges
 
 #include <bits/stdc++.h>
-using namespace std ;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+using namespace std;
 
-void createTree(TreeNode *root, vector<int> &data);
+int pi = 0;
 
-int main () {
-    vector<int> data;
-    int x;
-    while(cin >> x) {data.emplace_back(x);}
-    if (data.size() < 6 || data.size() > 20) {
-        cout << "Invalid input\n";
-        return 0; 
+void preorder_init(int *inorder, int *postorder, int ini, int ine, stack<int> &st, map<int, int> mp)
+{
+    if (ini > ine)
+        return;
+    int num = postorder[pi];
+    int inind = mp[num];
+    pi--;
+    preorder_init(inorder, postorder, inind + 1, ine, st, mp);
+    preorder_init(inorder, postorder, ini, inind - 1, st, mp);
+    st.push(num);
+}
+
+void print_preorder(int *inorder, int *postorder, int n)
+{
+    int k = n;
+    pi = k - 1;
+    map<int, int> mp;
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+        mp[inorder[i]] = i;
+    preorder_init(inorder, postorder, 0, k - 1, st, mp);
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
     }
-    TreeNode *root;
-    createTree(root, data);
+}
+
+int main()
+{
+    string in, post;
+    getline(cin, in);
+    getline(cin, post);
+    string ino[in.length()], posto[in.length()];
+    int ini = 0;
+    string word = "";
+    for (int i = 0; i < in.length(); i++)
+    {
+        char ch = in[i];
+        if (ch != ' ')
+            word += ch;
+        else
+        {
+            ino[ini++] = word;
+            word = "";
+        }
+    }
+    ino[ini++] = word;
+    word = "";
+    int posti = 0;
+    for (int i = 0; i < post.length(); i++)
+    {
+        char ch = post[i];
+        if (ch != ' ')
+            word += ch;
+        else
+        {
+            posto[posti++] = word;
+            word = "";
+        }
+    }
+    posto[posti++] = word;
+    if (ini >= 3 and ini <= 10)
+    {
+        int inorder[ini], postorder[posti];
+        for (int i = 0; i < ini; i++)
+        {
+            string num = ino[i];
+            int n = 0;
+            for (int j = 0; j < num.length(); j++)
+            {
+                n = n * 10 + (num[j] - 48);
+            }
+            inorder[i] = n;
+        }
+        for (int i = 0; i < posti; i++)
+        {
+            string num = posto[i];
+            int n = 0;
+            for (int j = 0; j < num.length(); j++)
+            {
+                n = n * 10 + (num[j] - 48);
+            }
+            postorder[i] = n;
+        }
+        print_preorder(inorder, postorder, ini);
+    }
+    else
+        cout << "Invalid input";
     return 0;
 }
--- DSA/41_Day/01_Traversal.cpp
+++ DSA/41_Day/01_Traversal.cpp
@@ -1,11 +1,107 @@
 // https://www.hackerrank.com/contests/cse205-16915-day41/challenges/traversal1-24906
 
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-int main () {
+int pi = 0;
+class TreeNode
+{
+public:
+    TreeNode *left, *right;
+    int val;
+    TreeNode(){};
+    TreeNode(int num)
+    {
+        left = NULL;
+        val = num;
+        right = NULL;
+    }
+};
+
+void pre(int *in, int *post, int ini, int ine, stack<int> &st, map<int, int> mp);
+void genPre(int *in, int *post, int *preorder, int n);
+int isData(int *arr, int a, int b, int num);
+
+TreeNode *genTree(int *in, int *preorder, int a, int b)
+{
+    static int pri = 0;
+    if (a > b) return NULL;
+    TreeNode *new_node;
+    new_node = new TreeNode(preorder[pri++]);
+    if (a == b)
+        return new_node;
+    int mid = isData(in, a, b, new_node->val);
+    new_node->left = genTree(in, preorder, a, mid - 1);
+    new_node->right = genTree(in, preorder, mid + 1, b);
+    return new_node;
+}
+
+void traverse(struct TreeNode *root)
+{
+    if (root != NULL)
+    {
+        traverse(root->left);
+        if (root->left == NULL and root->right == NULL)
+            cout << root->val << " ";
+        traverse(root->right);
+    }
+    return;
+}
+
+int main()
+{
     int n;
     cin >> n;
-    
+    int post[n], in[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> post[i];
+        in[i] = post[i];
+    }
+    sort(in, in + n);
+    int preorder[n];
+    genPre(in, post, preorder, n);
+    struct TreeNode *root = genTree(in, preorder, 0, n - 1);
+    traverse(root);
     return 0;
+}
+
+int isData(int *arr, int a, int b, int num)
+{
+    for (int i = a; i <= b; i++)
+    {
+        if (arr[i] == num)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void genPre(int *in, int *post, int *preorder, int n)
+{
+    int k = n;
+    pi = k - 1;
+    map<int, int> mp;
+    stack<int> st;
+    for (int i = 0; i < n; i++) mp[in[i]] = i;
+    pre(in, post, 0, k - 1, st, mp);
+    int i = 0;
+    while (!st.empty())
+    {
+        preorder[i] = st.top();
+        i++;
+        st.pop();
+    }
+}
+
+void pre(int *in, int *post, int ini, int ine, stack<int> &st, map<int, int> mp)
+{
+    if (ini > ine) return;
+    int num = post[pi];
+    int inind = mp[num];
+    pi--;
+    pre(in, post, inind + 1, ine, st, mp);
+    pre(in, post, ini, inind - 1, st, mp);
+    st.push(num);
 }
